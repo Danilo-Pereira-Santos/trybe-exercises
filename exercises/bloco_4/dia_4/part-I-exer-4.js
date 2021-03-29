086290882b22b48bca41ab6144b2dc226d2969cd
Danilo Pereira <danilodps22@hotmail.com>
2021-03-29 19:05:54 -0300

    Part I - exercicio 4: mostrar os valores das chaves do objeto com for/in (correçao)

--- exercises/bloco_4/dia_4/part-I-exer-4.js
+++ exercises/bloco_4/dia_4/part-I-exer-4.js
@@ -2,22 +2,11 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
-
-let info_2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-};
-
+  };
+  
 info.recorrente = 'Sim';
-info_2.recorrente = 'Sim';
 
 
 for (let key in info){
-  if (info[key] === info_2[key]){
-    console.log('Ambos recorrentes');
-  }else{
-    console.log(info[key],'e',info_2[key]);
-  }
+  console.log(key);
 }
