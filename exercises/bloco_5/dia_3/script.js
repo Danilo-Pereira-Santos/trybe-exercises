8a1b932092856f7251be47f6e291cbd1527675d0
Danilo Pereira <danilodps22@hotmail.com>
2021-04-07 22:28:48 -0300

    Exercicio 3

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -16,25 +16,28 @@ createDaysOfTheWeek();
 // Escreva seu código abaixo.
 
 // 1
-  
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
 let mounth = document.querySelector('#days');
 
-for (let index = 0; index < dezDaysList.length; index += 1){
-  let day = document.createElement('li');
-  day.innerText = dezDaysList[index];
-  day.className = 'day';
+function creatMounth(){
+ 
+  for (let index = 0; index < dezDaysList.length; index += 1){
+    let day = document.createElement('li');
+    day.innerText = dezDaysList[index];
+    day.className = 'day';
+    
+    if (day.innerText === '24' || day.innerText === '25' || day.innerText === '31'){
+      day.className += ' holiday';
+    }
   
-  if (day.innerText === '24' || day.innerText === '25' || day.innerText === '31'){
-    day.className += ' holiday';
+    if (day.innerText === '4' || day.innerText === '11' || day.innerText === '18' || day.innerText === '25'){
+      day.className += ' friday';
+    }
+    mounth.appendChild(day);
   }
-
-  if (day.innerText === '4' || day.innerText === '11' || day.innerText === '18' || day.innerText === '25'){
-    day.className += ' friday';
-  }
-  mounth.appendChild(day);
 }
 
+creatMounth();
 
 // Exercicio 2 
 
@@ -48,3 +51,20 @@ function buttonHoliday(ButtonName){
 }
 
 buttonHoliday('Feriados');
+
+// Exercicio 3
+let btnHoliday = document.querySelector('#btn-holiday');
+let holidayDays = document.querySelectorAll('.holiday');
+let cinza = 'rgb(238, 238, 238)';
+let azul = 'rgb(100, 255, 255)';
+
+btnHoliday.addEventListener('click', function (){
+  for (let index = 0; index < holidayDays.length; index += 1){    
+    if(holidayDays[index].style.backgroundColor !== azul){
+      holidayDays[index].style.backgroundColor = azul;
+    }else{
+      holidayDays[index].style.backgroundColor = cinza;
+    }
+  }
+
+});
